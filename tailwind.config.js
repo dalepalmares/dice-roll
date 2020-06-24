0c05a732d292eea3529a61b9173801001903e7e8
yelaD <dale@internold.com>
2020-06-24 15:08:39 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    }
+  },
+  variants: {
+    backgroundColor: ["responsive", "hover", "focus", "active"],
+  },
+  plugins: [],
+};
